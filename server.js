d9446ee84db0d22e616d67376eb56a2f05e6a854
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-10 17:09:40 -0500

    build folder is docs not client/build

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("docs"));
 }
 // Add routes
 app.use(routes);
