17f34cfdbb1e1e2b00ed4972c1eada6fd408e86d
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-10 19:57:53 -0500

    uploading build

--- docs/static/js/main.dc1fb2df.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprzelazny-react-profile"]=this["webpackJsonprzelazny-react-profile"]||[]).push([[0],{10:function(e,n,r){},11:function(e,n,r){"use strict";r.r(n);var c=r(0),t=(r(1),r(3)),o=r.n(t);r(9),r(10);var a=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("header",{className:"App-header",children:Object(c.jsx)("h1",{children:"Portfolio Page coming soon"})})})};o.a.render(Object(c.jsx)(a,{}),document.getElementById("root"))},9:function(e,n,r){}},[[11,1,2]]]);
-//# sourceMappingURL=main.dc1fb2df.chunk.js.map
