af87748fbe52dea35fceb377d7d883083686015c
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-11 20:17:03 -0500

    basic setup without react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
@@ -11,16 +10,11 @@ const BASE_NAME = "/";
 
 function App() {
   return (
-    <Router basename={`${BASE_NAME}`}>
+    <div>
       <Nav />
-      <Switch>
-        <Route exact path="/" component={About} />
-        <Route exact path="/Projects/" component={Projects} />
-        <Route exact path="/Projects/:id" component={Projects} />
-        <Route exact path="/Contact/" component={Contact} />
-        <Route component={NoMatch} />
-      </Switch>
-    </Router>
+      <About />
+    </div>
+
   );
 }
 
